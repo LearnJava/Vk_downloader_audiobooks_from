3856faad8b3b0fbc8cdc55b8655fd985eeb71ce8
konstantin <konstantin-spb@bk.ru>
2022-11-02 10:49:43 +0300

    23

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,17 @@
+import com.codeborne.selenide.CollectionCondition
 import com.codeborne.selenide.Condition
-import com.codeborne.selenide.Configuration
 import com.codeborne.selenide.Selectors
-import com.codeborne.selenide.Selenide
-import com.codeborne.selenide.Selenide.`$$`
-import com.codeborne.selenide.Selenide.open
+import com.codeborne.selenide.Selectors.byId
+import com.codeborne.selenide.Selenide.*
+import com.codeborne.selenide.SelenideElement
+import com.codeborne.selenide.WebDriverRunner.getWebDriver
 import com.codeborne.selenide.WebDriverRunner.setWebDriver
 import com.codeborne.selenide.ex.ElementNotFound
+import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.interactions.Actions
 import java.io.File
 import java.time.Duration
 
@@ -20,61 +23,93 @@ fun main() {
 }
 
 fun startHere() {
-    System.setProperty(
-        "webdriver.chrome.driver",
-        "C:\\Users\\konstantin\\IdeaProjects\\Vk_downloader_audiobooks_from\\chromedriver_win32\\chromedriver.exe"
-    )
-
-//    Configuration.browser = "chrome"
-//    Configuration.browserBinary = "C:\\Users\\User\\IdeaProjects\\Vk_downloader_audiobooks_from\\utils\\chrlauncher-win64-stable-ungoogled\\bin\\chrome.exe"
-    val options = ChromeOptions()
-    options.addExtensions(File("vkdownloader.crx"))
-    val webDriver: WebDriver = ChromeDriver(options)
-    setWebDriver(webDriver)
+    firstConfiguration()
     open("https://www.vk.com/")
 
-//    Selenide.`$`(Selectors.byXpath("//*[@id="index_email"]"))
-//        .shouldBe(Condition.visible, Duration.ofSeconds(10))
-//        .click()
+    checkAuthenticationAndLoginIfNeed()
 
-//    //*[@id="index_login"]/div/form/button[1]
-    println("Press any key.")
-    readLine()
-    println("Thank`s")
+    open("https://vk.com/best_audiobooks")
 
-    var downloadButtons = `$$`(Selectors.byXpath("//div[@class=\"vkd_download_all_btn\"]"))
+    //Ищем иконку поиска с лупой и кликаем по ней
 
-    while (true) {
+    for (title in File("titles.txt").readLines()) {
+        activateFindField()
+        sendSearchQuery(title)
+
+        val downloadButtons: List<SelenideElement>
         try {
-            if (downloadButtons.isNotEmpty()) {
-//                downloadButtons.last().click()
-                for (button in downloadButtons.chunked(5).first()) {
-                    button.click()
-                }
-
-                while (true) {
-                    try {
-                        Selenide.`$`(Selectors.byXpath("//div[@class=\"vkd_download_all_btn vkd_downloading\"]"))
-                            .should(
-                                Condition.exist, Duration.ofSeconds(4)
-                            )
-                    } catch (e: ElementNotFound) {
-                        break
-                    }
-                }
-
-                downloadButtons = `$$`(Selectors.byXpath("//div[@class=\"vkd_download_all_btn\"]"))
-                println("Качаем новую книгу.")
-            }
-        } catch (e: Exception) {
-
+            downloadButtons = `$$`(Selectors.byXpath("//div[@id='page_search_posts']//div[@class=\"vkd_download_all_btn\"]")).shouldBe(
+                CollectionCondition.sizeGreaterThan(0), Duration.ofSeconds(4)
+            )
+        } catch (e: com.codeborne.selenide.ex.ElementNotFound) {
+            continue
         }
-//        else {
-//            break
-//        }
+
+
+        for (button in downloadButtons) {
+            val actions = Actions(getWebDriver())
+            actions.moveToElement(button).build().perform()
+
+            val ex: JavascriptExecutor = getWebDriver() as JavascriptExecutor
+            ex.executeScript("arguments[0].click()", button)
+        }
+
+        while (true) {
+            try {
+                `$`(Selectors.byXpath("//div[@class=\"vkd_download_all_btn vkd_downloading\"]"))
+                    .should(
+                        Condition.exist, Duration.ofSeconds(4)
+                    )
+
+            } catch (e: ElementNotFound) {
+                break
+            }
+        }
     }
 
-
-
     println()
+}
+
+private fun sendSearchQuery(queryString: String) {
+    //Вводим поисковый текст в поле и жмём Enter
+    val searchElem =
+        `$`(byId("wall_search")).shouldBe(Condition.appear, Duration.ofSeconds(5))
+    searchElem.setValue(queryString).shouldBe(Condition.appear)
+    `$`(Selectors.byXpath("//div[@class='ui_search_reset_close']")).shouldBe(
+        Condition.appear,
+        Duration.ofSeconds(5)
+    )
+    searchElem.pressEnter()
+}
+
+private fun activateFindField() {
+    `$`(Selectors.byXpath("//*[@id=\"wall_tabs\"]/li[@class='ui_tab_search_wrap']/a"))
+        .shouldBe(Condition.appear, Duration.ofSeconds(5))
+        .pressEnter()
+}
+
+private fun firstConfiguration() {
+    System.setProperty(
+        "webdriver.chrome.driver",
+        "chromedriver.exe"
+    )
+//    Configuration.browser = "chrome"
+//    Configuration.browserBinary = "C:\\Users\\User\\IdeaProjects\\Vk_downloader_audiobooks_from\\utils\\chrlauncher-win64-stable-ungoogled\\bin\\chrome.exe"
+    val chromeOptions = ChromeOptions()
+    chromeOptions.addArguments("--user-data-dir=C:\\Users\\konstantin\\AppData\\Local\\Google\\Chrome\\User Data")
+    chromeOptions.addArguments("--profile-directory=Profile 1")
+    chromeOptions.addExtensions(File("vkdownloader.crx"))
+    val webDriver: WebDriver = ChromeDriver(chromeOptions)
+    setWebDriver(webDriver)
+}
+
+fun checkAuthenticationAndLoginIfNeed() {
+    if (!`$`(byId("top_profile_link")).exists()) {
+        //login
+        `$`(byId("index_email")).setValue("79184509096").pressEnter()
+        `$`(Selectors.byXpath("//input[@name='password']")).shouldBe(
+            Condition.appear,
+            Duration.ofSeconds(10)
+        ).setValue("Itisagooddaytofun232323").pressEnter()
+    }
 }
